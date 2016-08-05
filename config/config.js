d486b3c555aa72f59271b9097077645291033251
qiushi <qiushi@qiushideMacBook-Pro.local>
2016-08-05 12:34:56 +0800

    登录注册

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
-var env = process.env.NODE_ENV || 'development';
+var env = process.env.NODE_ENV || 'production';
 var config = null;
 if(null === config){
-	if(env === 'development'){
+	if(env === 'production'){
 		config = {
 			url:'http://www.baidu.com',
 			server: "http://10.251.1.46:8080"
@@ -11,6 +11,11 @@ if(null === config){
 		config = {
 			url:'http://www.jikexueyuan.com',
 			server: "http://123.56.126.231:8080"
+		}
+	}else if(env === 'dev'){
+		config = {
+			url:'http://www.jikexueyuan.com',
+			server: "http://192.168.1.108:8081"
 		}
 	}else{
 		config = {
--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,113 @@
-var express = require('express');
+var express = require('express')
 var config = require('../config/config.js')
 var request = require('request')
-var router = express.Router();
+var pbkdf2 = require('pbkdf2')
+var router = express.Router()
 
 /* GET home page. */
+
+router.get('/login', function(req, res) {
+    res.render('login')
+})
+
+router.post('/login', function(req, res) {
+    var name = req.body.name
+    var password = req.body.password
+    var requestDate = {
+        password: pbkdf2.pbkdf2Sync(password, 'salt123', 1, 32, 'sha512').toString('hex'),
+        account: name
+    }
+    console.log(requestDate)
+    request.post({
+            url: config.server + '/admin/login',
+            form: requestDate
+        },
+        function(err, response, body){
+            console.log('error', err)
+            body = JSON.parse(body)
+            console.log(body)
+            if (!err && response.statusCode == 200) {
+                if(body.ret) {
+                    console.log('========>',response.headers.token)
+                    res.cookie('islogin', response.headers.token, {maxAge: 60 * 1000})
+                    res.redirect('/')
+                }
+            }
+    })
+})
+
+router.get('/register', function(req, res) {
+    res.render('register')
+})
+
+router.post('/register', function(req, res) {
+    var name = req.body.name
+    var password = req.body.password
+    var adminType = req.body.adminType
+
+    var requestDate = {
+        password: pbkdf2.pbkdf2Sync('password', 'salt123', 1, 32, 'sha512').toString('hex'),
+        account: name,
+        adminType: adminType
+    }
+    console.log(requestDate)
+
+    request.post({
+            url: config.server + '/admin/createAdmin',
+            form: requestDate
+        },
+        function(err, response, body){
+            console.log('error', err)
+            body = JSON.parse(body)
+            if (!err && response.statusCode == 200) {
+                if(body.ret) {
+                    console.log('========>',response.headers.token)
+                    res.cookie('islogin', response.headers.token, {maxAge: 60 * 1000})
+                    res.redirect('/')
+                }
+            }
+    })
+})
+
+
+
+router.use(function(req, res, next){
+    if(req.cookies.islogin) return next()
+    return res.redirect('/login')
+})
+router.get('/logout', function(req, res) {
+    res.cookie('islogin', '', {maxAge: 60 * 1000})
+    res.redirect('/')
+})
+
 router.get('/', function(req, res, next) {
-    request.get(config.server + '/admin/monitor/overallCount', function(error, response, data) {
+    request.get({
+            url: config.server + '/admin/monitor/overallCount', 
+            headers: {
+                token: req.cookies.islogin
+            }
+        }, function(error, response, body) {
+
         if (!error && response.statusCode == 200) {
-            data = JSON.parse(data);
-            console.log(data)
-            res.render('index', { userCount: data.data.userCount , paperCount: data.data.paperCount, projectCount: data.data.projectCount });
+            body = JSON.parse(body);
+            console.log(body)
+            res.cookie('islogin', response.headers.token, {maxAge: 60 * 1000})
+            res.render('index', { userCount: body.data.userCount , paperCount: body.data.paperCount, projectCount: body.data.projectCount });
         }
     });
 });
 
 // 加载项目管理
 router.get('/project', function(req, res, next) {
-	console.log(config.server)
-	request.get(config.server + '/admin/project/list', function(error, response, data) {
+	request.get({
+            url: config.server + '/admin/project/list',
+            headers: {
+                token: req.cookies.islogin
+            }
+        }, function(error, response, data) {
         if (!error && response.statusCode == 200) {
             data = JSON.parse(data);
+            res.cookie('islogin', response.headers.token, {maxAge: 60 * 1000})
             res.render('projectAll', { list: data.data });
         }
     });
@@ -27,10 +115,16 @@ router.get('/project', function(req, res, next) {
 // 查询项目 （通过NO查询）
 router.get('/getProjectNo', function (req, res){
 	var projectNo = req.query.projectNo;
-    request({ url: config.server + '/admin/project/projectDetail?projectNo=' + projectNo }, function(error, response, body) {
+    request({
+            url: config.server + '/admin/project/projectDetail?projectNo=' + projectNo,
+            headers: {
+                token: req.cookies.islogin  
+            }
+        }, function(error, response, body) {
         if (!error && response.statusCode == 200) {
             body = JSON.parse(body);
             console.log(body)
+            res.cookie('islogin', response.headers.token, {maxAge: 60 * 1000})
             res.render('projectinfo', body.data);
         }
     })
@@ -38,15 +132,19 @@ router.get('/getProjectNo', function (req, res){
 // 项目完成度
 router.get('/getProjectCompleted', function(req, res) {
     var projectNo = req.query.projectNo
-    request({url : config.server + '/admin/monitor/getAllUserStatusByProjectNo?projectNo='+ projectNo}, function(error, response, body) {
+    request({
+            url : config.server + '/admin/monitor/getAllUserStatusByProjectNo?projectNo='+ projectNo,
+            headers: {
+                 token: req.cookies.islogin 
+            }
+        }, function(error, response, body) {
         if (!error && response.statusCode == 200) {
             body = JSON.parse(body);
             var keys = []
             for (obj in body.data) {
                 keys.push(obj)
             }
-            console.log(keys)
-            console.log(body.data)
+            res.cookie('islogin', response.headers.token, {maxAge: 60 * 1000})
             res.render('project', { data: body.data, mapKeys: keys});
         }
     })
@@ -64,6 +162,7 @@ router.post('/insertProject', function(req, res) {
     console.log(data);
     var options = {
         headers: {
+            'token': req.cookies.islogin,
             'Content-Type': 'application/json',
             'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.71 Safari/537.36'
         },
@@ -74,6 +173,7 @@ router.post('/insertProject', function(req, res) {
     };
     request(options, function(error, response, data) {
         if (!error && response.statusCode == 200) {
+            res.cookie('islogin', response.headers.token, {maxAge: 60 * 1000})
             res.json('ok');
         }
     });
@@ -81,11 +181,17 @@ router.post('/insertProject', function(req, res) {
 
 // 获取action列表
 router.get('/getActionList', function(req,res) {
-    request.get(config.server + '/admin/action/list', function(error, response, info) {
+    request.get({
+        url: config.server + '/admin/action/list',
+        headers: {
+            token: req.cookies.islogin
+        }
+    }, function(error, response, info) {
         if (error) res.json({ error: error });
         if (!error && response.statusCode == 200) {
             info = JSON.parse(info);
             console.log(info);
+            res.cookie('islogin', response.headers.token, {maxAge: 60 * 1000})
             res.json(info.data);
         }
     })
@@ -93,9 +199,15 @@ router.get('/getActionList', function(req,res) {
 
 // 试卷列表
 router.get('/list', function(req, res, next) {
-	request.get(config.server + '/admin/paper/list?paperName=', function(error, response, data) {
+	request.get({
+        url: config.server + '/admin/paper/list?paperName=',
+        headers: {
+            token: req.cookies.islogin
+        }
+    }, function(error, response, data) {
         if (!error && response.statusCode == 200) {
             data = JSON.parse(data)
+            res.cookie('islogin', response.headers.token, {maxAge: 60 * 1000})
             res.render('questionnaires', { list: data.data })
         }
     })
@@ -108,9 +220,15 @@ router.get('/new', function(req, res) {
 //得到试卷
 router.get('/getQuestion', function(req, res) {
     var paperId = req.query.paperId
-    request.get(config.server + '/admin/paper/readDetail?paperId=' + paperId, function(err, response, data) {
+    request.get({
+        url: config.server + '/admin/paper/readDetail?paperId=' + paperId,
+        headers: {
+            token: req.cookies.islogin
+        }
+    }, function(err, response, data) {
         if (!err && response.statusCode == 200) {
             data = JSON.parse(data);
+            res.cookie('islogin', response.headers.token, {maxAge: 60 * 1000})
             res.render('questionnaire', { data: data.data });
         }
     });
@@ -122,6 +240,7 @@ router.post('/insert', function(req, res) {
     var data = JSON.parse(req.body.data);
     var options = {
         headers: {
+            'token': req.cookies.islogin,
             'Content-Type': 'application/json',
             'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.71 Safari/537.36'
         },
@@ -132,6 +251,7 @@ router.post('/insert', function(req, res) {
     };
     request(options, function(error, response, data) {
         if (!error && response.statusCode == 200) {
+            res.cookie('islogin', response.headers.token, {maxAge: 60 * 1000})
             res.render('admin', {});
         }
     });
